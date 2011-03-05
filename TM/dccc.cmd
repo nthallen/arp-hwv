887795cc18869b0289ece751831ff09ac830ed2b
Norton Allen <allen@huarp.harvard.edu>
2011-03-05 20:40:51 +0000

    Renumber commands

--- TM/dccc.cmd
+++ TM/dccc.cmd
@@ -11,18 +11,18 @@
 # : Preamp SetGain &preampgain * { if_dccc.Turf( "M%s\n", $3 ); }
   ;
 &on_off_cmd <int>
-  : Lamp Module A { $0 = 0; }
-  : Lamp Module B { $0 = 1; }
+  : Lamp Module A { $0 = 7; }
+  : Lamp Module B { $0 = 0; }
   : Lamp Module C { $0 = 2; }
   : Lab Command 1 { $0 = 3; }
   : Lab Command 2 { $0 = 4; }
-  : Mirror Heat Zone 1 { $0 = 5; }
-  : Mirror Heat Zone 2 { $0 = 6; }
-  : Mirror Heat Zone 3 { $0 = 7; }
-  : Mirror Heat Zone 4 { $0 = 8; }
-  : Air A { $0 = 9; }
-  : Air B { $0 = 10; }
-  : Cmd_11 { $0 = 11; }
+  : Mirror Heat Zone 1 { $0 = 6; }
+  : Mirror Heat Zone 2 { $0 = 8; }
+  : Mirror Heat Zone 3 { $0 = 10; }
+  : Mirror Heat Zone 4 { $0 = 11; }
+  : Air A { $0 = 12; }
+  : Air B { $0 = 14; }
+  : Cmd_11 { $0 = 15; }
   : QCLI Reset { $0 = 24; }
   : Chopper A { $0 = 25; }
   : Chopper B { $0 = 26; }
