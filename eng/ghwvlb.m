4f802279691a362a1c759ef6f634e25e99947c42
cvs2svn <allen@huarp.harvard.edu>
2011-04-22 19:54:32 +0000

    This commit was manufactured by cvs2svn to create tag 'HWV_0_8'.
    Sprout from master 2011-04-22 19:54:31 UTC Norton Allen <allen@huarp.harvard.edu> 'SPV_P'
    Delete:
        eng/.cvsignore
        eng/genui.txt
        eng/getrun
        eng/ghwvc.m
        eng/ghwvcommands.m
        eng/ghwvcounts.m
        eng/ghwvctstat.m
        eng/ghwvdacs.m
        eng/ghwvf.m
        eng/ghwvg.m
        eng/ghwvgd.m
        eng/ghwvhhh.m
        eng/ghwvhhhpt.m
        eng/ghwvla.m
        eng/ghwvlb.m
        eng/ghwvlc.m
        eng/ghwvmz.m
        eng/ghwvp.m
        eng/ghwvqcli.m
        eng/ghwvsdp.m
        eng/ghwvspv.m
        eng/ghwvssp.m
        eng/ghwvt.m
        eng/phwvca.m
        eng/phwvcl.m
        eng/phwvclab.m
        eng/phwvcmz.m
        eng/phwvcommandsds.m
        eng/phwvcommandss.m
        eng/phwvcountsa.m
        eng/phwvcountsb.m
        eng/phwvcountsc.m
        eng/phwvcs.m
        eng/phwvctstatc.m
        eng/phwvctstatct1.m
        eng/phwvctstatct2.m
        eng/phwvdacsp.m
        eng/phwvdacsrh.m
        eng/phwvdacss.m
        eng/phwvdacst.m
        eng/phwvfpib.m
        eng/phwvfpif.m
        eng/phwvfsfb.m
        eng/phwvft.m
        eng/phwvgais.m
        eng/phwvgdp.m
        eng/phwvgdt.m
        eng/phwvgioss.m
        eng/phwvgsws.m
        eng/phwvhhhi.m
        eng/phwvhhhlte.m
        eng/phwvhhhptp.m
        eng/phwvhhhptrh.m
        eng/phwvhhhptt.m
        eng/phwvhhht.m
        eng/phwvlac.m
        eng/phwvlairv.m
        eng/phwvlap.m
        eng/phwvlarf.m
        eng/phwvlasp.m
        eng/phwvlat.m
        eng/phwvlbirv.m
        eng/phwvlbp.m
        eng/phwvlbrf.m
        eng/phwvlbsp.m
        eng/phwvlbt.m
        eng/phwvlcirv.m
        eng/phwvlcp.m
        eng/phwvlcrf.m
        eng/phwvlcsp.m
        eng/phwvlct.m
        eng/phwvmzct.m
        eng/phwvmzs.m
        eng/phwvmzt.m
        eng/phwvps.m
        eng/phwvpt.m
        eng/phwvqcliqcli.m
        eng/phwvqclis.m
        eng/phwvqcliw.m
        eng/phwvsdpdp.m
        eng/phwvsdpp.m
        eng/phwvsdpt.m
        eng/phwvspvp.m
        eng/phwvspvrh.m
        eng/phwvspvs.m
        eng/phwvspvt.m
        eng/phwvsspn.m
        eng/phwvsspovf.m
        eng/phwvssps.m
        eng/phwvsspsn.m
        eng/phwvsspstatus.m
        eng/phwvsspt.m
        eng/phwvsspts.m
        eng/phwvtb.m
        eng/phwvtp.m
        eng/phwvts.m
        eng/ui_hwv.m

--- eng/ghwvlb.m
+++
@@ -1,5 +0,0 @@
-function fig = ghwvlb(varargin);
-% ghwvlb(...)
-% Lamp B
-ffig = ne_group(varargin,'Lamp B','phwvlbt','phwvlbp','phwvlbrf','phwvlbirv','phwvlbsp');
-if nargout > 0 fig = ffig; end
--- eng/ghwvlc.m
+++
@@ -1,5 +0,0 @@
-function fig = ghwvlc(varargin);
-% ghwvlc(...)
-% Lamp C
-ffig = ne_group(varargin,'Lamp C','phwvlcp','phwvlcrf','phwvlcirv','phwvlct','phwvlcsp');
-if nargout > 0 fig = ffig; end
