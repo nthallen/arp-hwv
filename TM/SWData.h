fc9a10c7e43d6c2438898230462601a493f82d70
Norton Allen <allen@huarp.harvard.edu>
2012-06-07 15:11:09 +0000

    Adding additions from V1_1_Optim test branch

--- TM/SWData.h
+++ TM/SWData.h
@@ -1,25 +1,19 @@
-/* swstat.h */
-#ifndef SWSTAT_H_INCLUDED
-#define SWSTAT_H_INCLUDED
+/* SWData.h */
+#ifndef SWDATA_H_INCLUDED
+#define SWDATA_H_INCLUDED
 
-typedef struct {
+typedef struct __attribute__((__packed__)) {
   unsigned short SWStat;
-} __attribute__((packed)) swstat_t;
-extern swstat_t SWData;
+} SWData_t;
+extern SWData_t SWData;
 
-#define SWS_OK 0
 #define SWS_TAKEOFF 1
-#define SWS_CLIMB 2
-#define SWS_DESCEND 3
 #define SWS_LAND 4
-
 #define SWS_DFB_START 10
 #define SWS_DFB_STOP 11
 #define SWS_DFB_HHH 12
-
 #define SWS_H2O_ENABLE 20
 #define SWS_H2O_DISABLE 21
-
 #define SWS_TIME_WARP 253
 #define SWS_READFILE 254
 #define SWS_SHUTDOWN 255
