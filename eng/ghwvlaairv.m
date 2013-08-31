8188fba8f9305847b8fc22d8ed5e1833ea4bc44d
Norton Allen <allen@huarp.harvard.edu>
2013-08-31 15:28:01 +0000

    RF filtering

--- eng/ghwvlaairv.m
+++ eng/ghwvlaairv.m
@@ -1,5 +1,5 @@
 function fig = ghwvlaairv(varargin);
 % ghwvlaairv(...)
 % A IRV
-ffig = ne_group(varargin,'A IRV','phwvlaairvirv','phwvlaairvdirvdt');
+ffig = ne_group(varargin,'A IRV','phwvlaairvrf','phwvlaairvrfbw','phwvlaairvirv','phwvlaairvdirvdt');
 if nargout > 0 fig = ffig; end
