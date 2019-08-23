5415ca0477b7535f0b91b99b9a430746cf9be467
Norton Allen <allen@huarp.harvard.edu>
2019-08-23 11:56:39 -0400

    Appropriate channels

--- eng/HHHeng2PT.m
+++ eng/HHHeng2PT.m
@@ -4,12 +4,12 @@ function HHHeng2PT
 cfg = load_ICOSfit_cfg;
 runs = ne_load_runsdir('HWV_Data_Dir');
 run = getrun(1);
-E = load_eng('hwveng_1.mat', runs, run);
+E1 = load_eng('hwveng_1.mat', runs, run);
 E8 = load_eng('hwveng_8.mat', runs, run);
-if isempty(E) || isempty(E8)
+if isempty(E1) || isempty(E8)
     return;
 end
-T1 = E.Thwveng_1;
+T1 = E1.Thwveng_1;
 T8 = E8.Thwveng_8;
 
 if strcmp(cfg.ScanDir,'SSPo')
@@ -18,8 +18,8 @@ if strcmp(cfg.ScanDir,'SSPo')
     % Note 1.5 added to zero on April 6, 2013!
     % PT.Tavg = interp1(T8,273.15 + (E8.HHH1T + E8.HHH2T)/2,T1,'linear'); %gas temp (in K) to use for fit
     PT.Tavg = interp1(T8,273.15 + E8.HHH1T*0.96228783 + 2.06939487,T1,'linear'); %gas temp (in K) to use for fit
-    PT.ScanNum = E.SSP_Num;
-    PT.QCLI_Wave = E.QCLI_Wave;
+    PT.ScanNum = E1.SSP_Num;
+    PT.QCLI_Wave = E1.QCLI_Wave;
 else
     PT.TPT = T8;
     PT.CellP = E8.HHH1P*201.5121-8.544; %cell pressure (in Torr) to use for fit 7/2013
@@ -27,8 +27,8 @@ else
     % PT.Tavg = interp1(T8,273.15 + (E8.HHH1T + E8.HHH2T)/2,T1,'linear'); %gas temp (in K) to use for fit
     % PT.Tavg = 273.15 + E8.HHH1T*0.96228783 + 2.06939487; %gas temp (in K) to use for fit
     PT.Tavg = 273.15 + (E8.HHH1T+E8.HHH2T)/2; % Uncalibrated 7/2013
-    PT.ScanNum = interp1(T1,E.SSP_Num,T8,'nearest','extrap');
-    PT.QCLI_Wave = interp1(T1,E.QCLI_Wave,T8,'nearest','extrap');
+    PT.ScanNum = interp1(T1,E1.SSP_Num,T8,'nearest','extrap');
+    PT.QCLI_Wave = interp1(T1,E1.QCLI_Wave,T8,'nearest','extrap');
 end
 save PT.mat -STRUCT PT
 
