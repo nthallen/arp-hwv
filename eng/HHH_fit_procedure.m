59442b1131ba5211cf868e86c446b485ac26efd5
Norton Allen <allen@huarp.harvard.edu>
2022-02-16 11:36:49 -0500

    HHH_fit_procedure.m

---
+++ eng/HHH_fit_procedure.m
@@ -0,0 +1,64 @@
+%%
+PTEref = '../210807.1F/PTE_fit.txt';
+PTEref_row = 15;
+PTE = load(PTEref);
+PTE_params = PTE(PTEref_row,4:end);
+clear PTE;
+%%
+waves_used;
+% Then set scans and run scanview(scans) to decide where to start
+%%
+scans = 1490:204299;
+PTEfile = 'PTE_gen_10Hz.txt';
+PTE_gen(scans,PTEfile,PTE_params);
+fprintf(1,'Now average\n');
+average_spectra2(1,PTEfile);
+%%
+edit_ICOSfit_cfg; % to switch to the averaged scans
+%%
+% define R1A that covers all the average data
+PTE = load('PTE_gen_10Hz.average1.txt');
+fitline('update_regions','R1A',PTE([1 end],1));
+clear PTE
+%%
+fitline % to initialize icosfit file
+%%
+% Update the fitfile name
+suffixes = 'R1.1Hz5p3er';
+fitfile = [ 'icosfit.' suffixes ];
+base = ['ICOSout.' suffixes ];
+%%
+% Update fitfile with standard tweaks:
+icosfit_reconfig(fitfile, '', ...
+  'Verbosity', '2', ...
+  'MaxEnsembleDriftPerScan', '5e-3 cm-1', ...
+  'MaxIterations', '20');
+%%
+% Now run icosfit outside Matlab
+%%
+% Take a first look to see how things went
+mixlines(base);
+S = ICOS_setup(base);
+figure; plot(S.scannum, S.nu_F0);
+%%
+% Do whatever we have to to fixup the 1 Hz fit, starting with:
+PTE_add_nu_F0(base);
+% And then trying to interpolate through regions where we lost the line
+%%
+% Now to the 10Hz fit:
+PTE10nu = PTE_add_nu_F0(base,'ifile','PTE_gen_10Hz.txt');
+PTE = load(PTE10nu);
+fitline('update_regions','R1',PTE([1 end],1));
+clear PTE
+%%
+edit_ICOSfit_cfg; % To switch back to 10Hz scans
+fitline
+%%
+fitfile = 'icosfit.R1.10Hz5p3er';
+icosfit_reconfig(fitfile, '', ...
+  'Verbosity', '2', ...
+  'MaxEnsembleDriftPerScan', '5e-3 cm-1', ...
+  'MaxIterations', '20', ...
+  'PTEFile',[PTE10nu ' + nu_F0']);
+%%
+% No fit at 10 Hz
