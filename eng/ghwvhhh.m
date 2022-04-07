2306d35054d293c8d0402c8038d934404029f9f1
Norton Allen <allen@huarp.harvard.edu>
2022-04-07 15:29:58 -0400

    genui cleanup

---
+++ eng/ghwvhhh.m
@@ -0,0 +1,5 @@
+function fig = ghwvhhh(varargin);
+% ghwvhhh(...)
+% icosfitd
+ffig = ne_group(varargin,'icosfitd','phwvhhhscan','phwvhhhp','phwvhhht','phwvhhhnu_f0','phwvhhhppm','phwvhhhfixflt','phwvhhhstatus');
+if nargout > 0 fig = ffig; end
